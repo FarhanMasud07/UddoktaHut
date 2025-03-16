9ec3f8338d33b511f817a8cdac6fed93d685838d
farhan-masud-1616 <farhan.masud@optimizely.com>
2025-03-16 23:23:33 +0600

    make output to extoprt static files

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,7 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  output: "export", // Ensure Next.js exports static files
+  trailingSlash: true, // Helps Cloudflare serve pages correctly
+};
 
 export default nextConfig;
