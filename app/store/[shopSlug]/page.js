0bb35ff45a5aac18f9eb21ff007774119fd6e70e
farhan-masud-1616 <farhan.masud@optimizely.com>
2025-03-17 12:33:16 +0600

    await params

--- app/store/[shopSlug]/page.js
+++ app/store/[shopSlug]/page.js
@@ -2,12 +2,13 @@ import { notFound } from "next/navigation";
 
 const validShops = ["shoporia", "salamkacchi", "shopup", "funnymg"];
 
-export default function ShopPage({ params }) {
-  if (!validShops.includes(params.shopName)) return notFound();
+export default async function ShopPage({ params }) {
+  const { shopName } = await params;
+  if (!validShops.includes(shopName)) return notFound();
   return (
     <div style={{ textAlign: "center", padding: "50px" }}>
-      <h1>Welcome to {params.shopName}s Shop</h1>
-      <p>Enjoy shopping at {params.shopName}.uddoktahut.com</p>
+      <h1>Welcome to {shopName}s Shop</h1>
+      <p>Enjoy shopping at {shopName}.uddoktahut.com</p>
     </div>
   );
 }
