b5822338a33d08bc79b7051d757f673d1de0d4df
farhan-masud-1616 <farhan.masud@optimizely.com>
2025-04-11 22:18:37 +0600

    changes the Get for logout

--- app/logout/route.js
+++ app/logout/route.js
@@ -2,28 +2,13 @@ import { CONFIG } from "@/lib/config";
 import { NextResponse } from "next/server";
 
 export async function GET() {
-  const response = NextResponse.redirect(
+  await fetch(`${process.env.NEXT_PUBLIC_BACKEND_API}/auth/logout`, {
+    method: "GET",
+    credentials: "include",
+  });
+  return NextResponse.redirect(
     `${
       CONFIG.isProd ? process.env.NEXT_PUBLIC_BASE_URL : "http://localhost:3000"
     }/login`
   );
-
-  response.cookies.set("accessToken", "", {
-    httpOnly: true,
-    secure: true,
-    sameSite: "None",
-    path: "/",
-  });
-
-  response.cookies.set("refreshToken", "", {
-    httpOnly: true,
-    secure: true,
-    sameSite: "None",
-    path: "/",
-  });
-
-  response.cookies.delete("accessToken");
-  response.cookies.delete("refreshToken");
-
-  return response;
 }
